6b530fcf5a7b87cd9a93fe48197fef51beed3509
EdBozhko <eduard.bozhko92@gmail.com>
2021-03-29 15:29:42 +0300

    finished task 3

---
+++ hw_14/task3.js
@@ -0,0 +1,5 @@
+// Задача. Знайти суму від’ємних елементів масиву.
+
+let arr = [0, 14, -5, 7, -3, 11, 6, -2]
+let filter = arr.filter((value) => value < 0)
+let result = document.write(filter.reduce((accumulator, currentValue) => accumulator + currentValue));
